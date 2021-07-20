54104b1cf94b4d527a4209eba23049a61deb7fc2
Christian Paranas <christiannparanas@gmail.com>
2021-07-20 18:54:04 +0800

    new db host

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 
 const db = mysql.createConnection({
 	multipleStatements: true,
-	host: 'freedb.tech',
-	user: 'freedbtech_christian',
-	password: 'christian',
-	database: 'freedbtech_Ecommerce'
+	host: 'sql200.epizy.com',
+	user: 'epiz_28143499',
+	password: '6Edw4ud5tiB4yP',
+	database: 'epiz_28143499_tindahan'
 })
 
 module.exports = db
